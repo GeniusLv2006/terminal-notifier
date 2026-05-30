d69c0877bf3ffb1c2b73c705f6ae0f35a0319a2e
Jack11111eee <1820060304@qq.com>
2026-05-30 15:04:41 +0800

    菜单栏图标统一:引用 menuBarIconSize(=22)并关抗锯齿
    updateIcon 不再硬编码 22,改用 Constants.menuBarIconSize(同步设为22保持外观);
    drawColoredCat 关闭抗锯齿,使菜单栏小猫与悬浮窗大猫一样像素硬边。

--- TerminalNotifier/App/Constants.swift
+++ TerminalNotifier/App/Constants.swift
@@ -5,7 +5,7 @@ enum Constants {
     static let terminalAppName = "Terminal"
     static let badgePollInterval: TimeInterval = 1.0
     static let defaultPetSize: CGFloat = 320
-    static let menuBarIconSize: CGFloat = 18
+    static let menuBarIconSize: CGFloat = 22
     static let defaultCooldown: Int = 10
     static let longWaitThreshold: TimeInterval = 120
 
